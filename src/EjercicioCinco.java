036bb47e75908c044477eaa05217ee4d14f76812
yonnii12 <yonnii401@hotmail.com>
2024-10-18 17:12:41 -0500

    Quinto ejercicio

---
+++ src/EjercicioCinco.java
@@ -0,0 +1,17 @@
+public class EjercicioCinco {
+    public static void main(String[] args) {
+
+//        Objetivo: Convertir un ArrayList en un array nativo de Java.
+//
+//                Instrucciones:
+//
+//        Crea un ArrayList de enteros con los valores del 1 al 5.
+//        Convierte este ArrayList en un array de tipo Integer[].
+//                Imprime el contenido del array usando un bucle for.
+
+        
+
+
+
+    }
+}
