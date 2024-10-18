46194d60633380edc17070f47f9565919c23cfea
yonnii12 <yonnii401@hotmail.com>
2024-10-18 17:43:18 -0500

    Final ejercicio

--- src/EjercicioCinco.java
+++ src/EjercicioCinco.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class EjercicioCinco {
     public static void main(String[] args) {
 
@@ -9,9 +12,20 @@ public class EjercicioCinco {
 //        Convierte este ArrayList en un array de tipo Integer[].
 //                Imprime el contenido del array usando un bucle for.
 
-        
+        List<Integer> enteros = new ArrayList<>();
 
+        for (int i = 1; i <= 5; i++) {
+            enteros.add(i);
+        }
+        System.out.println(enteros);
 
+        Integer[] array = new Integer[enteros.size()];
+        array = enteros.toArray(array);
+
+        System.out.println("Contenido del array es el siguiente ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
 
     }
 }
